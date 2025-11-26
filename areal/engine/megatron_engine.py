bb6008be967941b142e71e5ba3536bcd3ca9c3ac
晓雷 <meizhiyu.mzy@antgroup.com>
2025-11-26 12:44:54 +0800

    .

--- areal/engine/megatron_engine.py
+++ areal/engine/megatron_engine.py
@@ -121,6 +121,7 @@ class MegatronEngine(TrainEngine):
         self.checkpointer = None
         self.seed = 0
         self.own_global_group = False
+        self.is_offload: bool = False
         self.enable_tree_training = self.mcore_config.enable_tree_training
 
     def initialize(
